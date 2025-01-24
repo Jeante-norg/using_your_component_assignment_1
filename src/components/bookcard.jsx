1903d734546fd599c9e3100b61746812633c6c8d
Tej <jupitersorbeet@gmail.com>
2025-01-24 12:27:33 +0530

    assignment completed

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,49 @@
 // write the book component code here
+const Bookcard = (props) => {
+  return (
+    <div style={styles.card}>
+      {/* <h1>goodbye world.</h1> */}
+      <img style={styles.img} src={props.image} alt="" />
+      <div>
+        <h2 style={styles.name}> {props.name}</h2>
+        <p style={styles.details}>
+          {props.genre}
+          <br /> {props.author}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  card: {
+    borderRadius: "15px",
+    border: "1px solid #ddd",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minWidth: "670px",
+    // margin: "1rem",
+    margin: "1rem auto",
+    padding: "15px",
+    gap: "25px",
+    backgroundColor: "#f9f9f9",
+    flexDirection: "row",
+  },
+  img: {
+    borderRadius: "10px",
+    height: "150px",
+    width: "150px",
+    objectFit: "cover",
+  },
+  name: {
+    fontWeight: "bold",
+    color: "#555",
+  },
+  details: {
+    flexDirection: "column",
+    color: "#555",
+  },
+};
+
+export default Bookcard;
